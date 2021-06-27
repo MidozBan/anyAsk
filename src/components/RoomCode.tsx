f7b00e0fb97625fc3ce060ff5849dd4b306fe055
Augusto Caires <augusto.caires@otempo.com.br>
2021-06-27 20:01:48 -0300

    primeiro commit

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,21 @@
+import copyImg from '../assets/images/copy.svg';
+import '../styles/room-code.scss'
+
+type RoomCodeProps = {
+    code: string; 
+}
+
+export function RoomCode (props: RoomCodeProps) {
+    function copyRoomCodeToClipboard() {
+        navigator.clipboard.writeText('-Md2aOnxU-ZxF3u0VvjB');
+    }
+    return (
+        <button className="room-code" onClick={copyRoomCodeToClipboard}>
+            <div>
+                <img src={copyImg} alt="Copy room code"/>
+                
+            </div>
+            <span> Sala #{props.code}</span>
+        </button>
+    )
+} 
